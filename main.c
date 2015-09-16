c421a2ba4b8aea1cddca00bff521c0e8ce58b028
Gael Roue <roue.g29@gmail.com>
2015-09-16 12:26:35 +0200

    test add main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char ** argv)
+{
+	if (argc < 2) {
+		exit(0);
+	}
+	unsigned char * frame = argv[1];
+	return 0;
+}
+
