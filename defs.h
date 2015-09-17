a0b103fdd83ccdc59c6a928059751f2733766eff
lo49 <loic.bouilly@gmail.com>
2015-09-17 12:01:06 +0200

    change defs.h

--- defs.h
+++ defs.h
@@ -1,7 +1,14 @@
 #ifndef DEFS_H
 #define DEFS_H 1
 
-typedef unsigned char uint8_t;
+/** Typedefs : **/
+typedef unsigned char 		uint8_t;
+typedef unsigned short 		uint16_t;
+typedef unsigned int		uint32_t;
 
+
+
+int send_uart(int len, uint8_t * frame);
+void chksum(uint8_t * frame);
 
 #endif
